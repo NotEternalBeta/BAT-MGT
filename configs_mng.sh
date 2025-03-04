222fe53a1cfc83433bb62d064bf52348f8965ff3
NotEternalBeta <maks.kokurin.02@mail.ru>
2025-03-04 22:37:34 +0300

    current profile index determination

--- configs_mng.sh
+++ configs_mng.sh
@@ -11,4 +11,33 @@ charge_threshold_configs=$(ls "$charge_threshold_profiles_path")
 #echo "Список файлов в папке основных конфигов: $main_configs"
 #echo "Список файлов в папке конфигов лимита заряда: $charge_threshold_configs"
 
+mkdir -p "$program_path/data"
+data_file="$program_path/data/data.txt"
+if [ ! -f "$program_path/data/data.txt" ]; then
+    touch $data_file
+fi
 
+current_profile=$(cat $data_file)
+current_profile_index=-1
+
+# return index profile from current data, otherwise -1
+get_current_main_profile_index() {
+    #current_profile_index=$((current_profile_index + 1))
+    local index=-1
+    if [ -f "$main_profiles_path/$current_profile" ]; then
+        for profile in "$main_profiles_path"/*.conf; do
+            index=$((index + 1))
+            #echo "$profile"
+            local profile_short="$(basename "$profile")"
+            #echo "$profile_short"
+            if [ "$profile_short" == "$current_profile" ]; then
+                #echo "eq"
+                break
+            fi
+        done
+    fi
+
+    echo "$index"
+}
+
+#echo $(get_current_main_profile_index)
