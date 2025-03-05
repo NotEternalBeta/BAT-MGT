83558494fd5d650dc0b1b0a9bc0ed71044f00d0c
NotEternalBeta <maks.kokurin.02@mail.ru>
2025-03-05 03:11:03 +0300

    profiles indexes

--- configs_mng.sh
+++ configs_mng.sh
@@ -6,6 +6,7 @@ data_path="$program_path/data"
 current_profiles_data="$data_path/current_profiles.txt"
 profiles_mapping_data="$data_path/profiles_mapping.txt"
 
+profiles_path="$program_path/profiles"
 main_profiles_path="$program_path/profiles/main"
 charge_threshold_profiles_path="$program_path/profiles/charge_threshold"
 
@@ -14,7 +15,10 @@ charge_threshold_profiles_path="$program_path/profiles/charge_threshold"
 #echo "Список файлов в папке основных конфигов: $main_configs"
 #echo "Список файлов в папке конфигов лимита заряда: $charge_threshold_configs"
 
+#profile_params=("file" "label" "caption")
+
 declare -A current_profiles
+declare -A profiles_indexes
 declare -A profiles_map
 
 init() {
@@ -36,10 +40,43 @@ read_profiles() {
 
     while IFS='=' read -r key value; do
         [[ -z "$key" || "$key" =~ ^# ]] && continue
-        profiles_map["$key"]="$value"
+        profiles_map["$key"]="$profiles_path/$value"
     done < "$profiles_mapping_data"
 }
 read_profiles
+
+set_indexes() {
+    for key in "${!profiles_map[@]}"; do
+        profiles_indexes["$key"]=-1
+        
+        if [[ ! -v current_profiles[$key] || -z ${current_profiles[$key]} ]]; then
+            #echo "was null: '$key' = '${current_profiles[$key]}'"
+            continue
+        fi
+
+        if [[ ! -f "${profiles_map[$key]}/${current_profiles[$key]}" ]]; then
+            #echo "No such file or directory."
+            continue
+        fi
+        
+        #echo "list: "
+        #ls "${profiles_map[$key]}"
+        #echo "end list"
+
+        for file in "${profiles_map[$key]}"/*.conf; do
+            #echo "Key type: $(declare -p key)"
+            profiles_indexes["$key"]=$((profiles_indexes["$key"]+1))
+            #echo "check: key: '$key' value: '${profiles_indexes[$key]}' in file '$file'"
+            #echo "result: ${profiles_indexes[$key]}"
+            local profile_name="$(basename "$file")"
+            if [ "$profile_name" == "${current_profiles[$key]}" ]; then
+                #echo "break on: '$profile_name'"
+                break;
+            fi
+        done
+    done
+}
+set_indexes
 
 print_profiles_data() {
     printf "Data from current_profiles.txt:\n"
@@ -52,7 +89,16 @@ print_profiles_data() {
     done
     printf "\n"
 }
-#print_profiles_data
+print_profiles_data
+
+print_indexes() {
+    printf "Printing indexes:\n"
+    for key in "${!profiles_indexes[@]}"; do
+        printf "'%s' = '%d'\n" "$key" "${profiles_indexes[$key]}"
+    done
+    printf "\n"
+}
+print_indexes
 
 key_main="main"
 key_charge_threshold="charge_threshold"
@@ -100,5 +146,3 @@ get_current_profile_index() {
     echo "$index"
 }
 echo $(get_current_profile_index)
-
-current_charge_thresh_profile=
